9989e1c6c95f351ee4d0845c351a151c0a861da9
xiaomufeng <573883891@qq.com>
2016-07-14 13:55:11 +0800

    delete annotate

--- src/main/java/com/java/annotation/MainTest.java
+++ src/main/java/com/java/annotation/MainTest.java
@@ -1,10 +1,3 @@
-/*
- * Copyright 2016 alibaba.com All right reserved. This software is the
- * confidential and proprietary information of alibaba.com ("Confidential
- * Information"). You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement you entered
- * into with alibaba.com.
- */
 package com.java.annotation;
 
 import java.lang.reflect.Field;
